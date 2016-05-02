ea1414fd68096c999e425581159589739e85b11a
crapthings <crapthings@gmail.com>
2016-05-02 22:46:16 +0800

    webpack 1x testfield

---
+++ readme.md
@@ -0,0 +1,36 @@
+## webpack 1x starter
+
+### ref
+
+https://github.com/babel/babel-loader
+
+https://github.com/ampedandwired/html-webpack-plugin
+
+https://github.com/webpack/style-loader
+
+https://github.com/webpack/css-loader
+
+https://github.com/shama/stylus-loader
+
+https://github.com/postcss/postcss-loader
+
+https://github.com/webpack/extract-text-webpack-plugin
+
+### common framework ref
+
+https://github.com/shakacode/bootstrap-loader
+
+### article
+
+https://medium.com/@rajaraodv/webpack-the-confusing-parts-58712f8fcad9#.pvjt1ymnb
+
+http://blog.thebakery.io/webpack-config-modules-directories-define/
+
+### need to test loader
+
+https://github.com/webpack/imports-loader
+
+##### probs to solve
+
+inline css injection bootstrap loader 非常不爽，会导致glitch
+探索更好的管理一些第三方像bootstrap这样的前端库
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+var path = require('path')
+
+var webpack = require('webpack')
+
+var webpackMajorVersion = require('webpack/package.json').version.split('.')[0]
+
+var htmlWebpackPlugin = require('html-webpack-plugin')
+
+var extractTextPlugin = require('extract-text-webpack-plugin')
+
+//
+var autoprefixer = require('autoprefixer')
+var postcssShort = require('postcss-short')
+
+//
+
+var babelLoader = {
+  test: /\.js$/,
+  loader: 'babel',
+  query: {
+    presets: ['es2015', 'stage-3']
+  },
+  exclude: /node_modules/
+}
+
+var cssLoader = {
+  test: /\.css$|.styl$/,
+  loader: extractTextPlugin.extract('css!postcss!stylus')
+}
+
+var jsonLoader = {
+  test: /\.json$/,
+  loader: 'json'
+}
+
+module.exports = {
+
+  entry: ['babel-polyfill', 'bootstrap-loader', './index.js'],
+
+  output: {
+    path: path.join(__dirname, '/bundle/webpack' + webpackMajorVersion),
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.css', '.styl'],
+    root: [
+      path.resolve(__dirname)
+    ]
+  },
+
+  module: {
+    loaders: [
+      babelLoader,
+      cssLoader,
+      jsonLoader, {
+        test: /\.(woff2?|svg)$/,
+        loader: 'url?limit=10000'
+      }, {
+        test: /\.(ttf|eot)$/,
+        loader: 'file'
+      },
+    ]
+  },
+
+  plugins: [
+
+    new htmlWebpackPlugin({
+      template: 'index.html',
+      inject: true,
+      hash: true
+    }),
+
+    new extractTextPlugin('bundle.css'),
+
+    new webpack.ProvidePlugin({
+      _: 'lodash',
+      moment: 'moment',
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+
+  ],
+
+  stylus: {
+    use: [require('nib')()],
+    import: ['~nib/lib/nib/index.styl']
+  },
+
+  postcss: function() {
+    return [autoprefixer, postcssShort]
+  }
+
+}
